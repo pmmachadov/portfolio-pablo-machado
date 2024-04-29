7d6bf0dcf982216e930d916449744e0d6ccc7284
Pablo Machado <100440808+pmmachadov@users.noreply.github.com>
2024-04-29 14:01:14 +0200

    Cards shown now in columns

--- sections/Projects.tsx
+++ sections/Projects.tsx
@@ -1,19 +1,30 @@
-
 import React from 'react';
 import ProjectCard from '@components/ProjectCard';
 import SectionCard from '@components/SectionCard';
 import { projectData } from '@sections/Me';
 import BgTitle from '@components/BgTitle';
+import containerStyle from '@styles/ProjectCard.module.sass';
 
 const Projects = () => {
+  const gridStyle = {
+    display: 'grid',
+    justifyContent: 'center',
+    gridTemplateColumns: 'repeat(3, 1fr)',
+    gap: '20px',
+    alignItems: 'stretch',
+    margin: '2rem 0'
+  };
+
+
   return (
     <SectionCard id='projects' title='PROJECTS' page='WORK'>
-      <div style={ { position: 'relative' } }>
+      <div style={ containerStyle }>
         <BgTitle title='WORK' />
-
-        { projectData.map((p, i) => (
-          <ProjectCard project={ p } key={ i } />
-        )) }
+        <div style={ gridStyle }>
+          { projectData.map((project) => (
+            <ProjectCard key={ project.title } project={ project } />
+          )) }
+        </div>
       </div>
     </SectionCard>
   );
